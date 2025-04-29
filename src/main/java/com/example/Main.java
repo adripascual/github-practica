10c2fe119969eee7e52f7692a4e9ff21c86d43e5
adripascual <adrianpascual@beentor.com>
2025-04-29 21:56:22 +0200

    Create Main.java

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Clase principal que contiene el método main
+ */
+public class Main {
+    /**
+     * Método principal que imprime un saludo
+     * @param args argumentos de línea de comandos
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Gaspar");
+    }
+}
